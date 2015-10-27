89966e1e25b6daab49ef6f9242493835dc67977f
resh <rs2715@stern.nyu.edu>
2015-10-27 19:20:02 +0000

    adding chronic_medicare app

--- mldjango/settings.py
+++ mldjango/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chronic_medicare',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -101,3 +102,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
